0b94438321cda75ff63daa76a5ec9d0adf0d2ff2
Admin <jeff.conrad@gmx.de>
2023-09-05 21:28:43 +0200

    import android.content.Context entfernt

--- app/src/main/java/com/example/summaryyoupython/MainActivity.kt
+++ app/src/main/java/com/example/summaryyoupython/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.summaryyoupython
 
 import android.content.ClipData
 import android.content.ClipboardManager
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
