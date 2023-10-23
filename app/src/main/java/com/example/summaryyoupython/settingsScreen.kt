d54b541f1931ac40bd2452c1c9b1465f15262181
Admin <jeff.conrad@gmx.de>
2023-10-23 11:05:52 +0200

    added version number in settings

--- app/src/main/java/com/example/summaryyoupython/settingsScreen.kt
+++ app/src/main/java/com/example/summaryyoupython/settingsScreen.kt
@@ -1,65 +1,38 @@
 package com.example.summaryyoupython
 
+import android.content.Context
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Info
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
-@Composable
-fun settingsScreenOld(modifier: Modifier = Modifier, navController: NavHostController) {
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .verticalScroll(rememberScrollState())
-            .fillMaxWidth(),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        IconButton(onClick = {navController.navigate("home")}, modifier = modifier.padding(start = 8.dp, top=55.dp)) {
-            Icon(Icons.Outlined.ArrowBack, contentDescription = "Arrow Back")
-        }
-        Column(
-            modifier = modifier
-                .padding(start=20.dp, end=20.dp, top=17.dp)
-        ) {
-            Text(
-                text = stringResource(id = R.string.settings),
-                style = MaterialTheme.typography.headlineLarge
-            )
-
-        }
-    }
-}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -100,6 +73,8 @@ fun settingsScreen(modifier: Modifier = Modifier, navController: NavHostControll
 @Composable
 fun ScrollContent(innerPadding: PaddingValues) {
     val range = 1..10
+    val context = LocalContext.current // Holen Sie sich den Context aus AmbientContext
+
 
     LazyColumn(
         modifier = Modifier
@@ -123,6 +98,18 @@ fun ScrollContent(innerPadding: PaddingValues) {
                 HorizontalDivider()
                 ListItem(
                     modifier = Modifier.fillMaxWidth(), // Optional, um die ListItem auf die volle Breite zu strecken
+                    headlineContent = { Text(stringResource(id = R.string.useUltraDark)) },
+                    supportingContent = { Text(stringResource(id = R.string.useUltraDarkDescription)) },
+                    trailingContent = {
+                        Switch(
+                            checked = true, // Hier können Sie den Zustand des Schalters anpassen
+                            onCheckedChange = { /* Hier können Sie die Logik für den Schalter einfügen */ }
+                        )
+                    }
+                )
+                HorizontalDivider()
+                ListItem(
+                    modifier = Modifier.fillMaxWidth(), // Optional, um die ListItem auf die volle Breite zu strecken
                     headlineContent = { Text("Github") },
                     leadingContent = {
                         Icon(
@@ -132,12 +119,28 @@ fun ScrollContent(innerPadding: PaddingValues) {
                     },
                     supportingContent = { Text(stringResource(id = R.string.githubDescription)) }
                 )
-                Text(text = "versionName")
+                Text(text = "Version ${getVersionName(context)}", modifier = Modifier.align(alignment = CenterHorizontally))
             }
         }
-
-        items(range.count()) { index ->
-            Text(text = "- List item number ${index + 1}")
-        }
     }
+}
+
+fun getVersionName(context: Context): String? {
+    val packageInfo: PackageInfo
+    packageInfo = try {
+        context.packageManager.getPackageInfo(context.packageName, 0)
+    } catch (e: PackageManager.NameNotFoundException) {
+        throw RuntimeException(e)
+    }
+    return packageInfo.versionName
+}
+
+fun getVersionCode(context: Context): Int {
+    val packageInfo: PackageInfo
+    packageInfo = try {
+        context.packageManager.getPackageInfo(context.packageName, 0)
+    } catch (e: PackageManager.NameNotFoundException) {
+        throw RuntimeException(e)
+    }
+    return packageInfo.versionCode
 }
