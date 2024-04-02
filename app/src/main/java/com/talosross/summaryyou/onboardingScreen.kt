14d169e982995bd4eeac1d275b98ce10b6c66ea4
talosross <talosross@gmail.com>
2024-04-02 02:41:37 +0200

    Added Skip option in OnboardingScreen

--- app/src/main/java/com/talosross/summaryyou/onboardingScreen.kt
+++ app/src/main/java/com/talosross/summaryyou/onboardingScreen.kt
@@ -3,6 +3,7 @@ package com.talosross.summaryyou
 import android.os.Build
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -79,12 +81,22 @@ fun OnboardingScreen(
                             .fillMaxWidth()
                             .padding(start = 12.dp, end = 12.dp, top = 15.dp)
                     )
-                    Button(
-                        modifier = Modifier
-                            .padding(vertical = 18.dp),
-                        onClick = { screen = 2 }
-                    ) {
-                        Text(text = stringResource(id = R.string.continueButton))
+                    Row() {
+                        FilledTonalButton(
+                            modifier = Modifier
+                                .padding(vertical = 18.dp),
+                            onClick = onContinueClicked
+                        ) {
+                            Text(text = stringResource(id = R.string.skip))
+                        }
+                        Spacer(modifier = modifier.padding(start = 12.dp))
+                        Button(
+                            modifier = Modifier
+                                .padding(vertical = 18.dp),
+                            onClick = { screen = 2 }
+                        ) {
+                            Text(text = stringResource(id = R.string.continueButton))
+                        }
                     }
                 }
 
