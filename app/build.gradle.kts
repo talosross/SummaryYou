bfc4f78c8e120ce19f66fe0010bef160cdd6859a
alex <kid1412621@gmail.com>
2025-07-19 21:45:52 +0800

    build: update pipeline chore: update outdated configs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
     }
-    kotlinOptions {
-        jvmTarget = "21"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21)
+        }
     }
     buildFeatures {
         compose = true
         buildConfig = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
     }
     packaging {
         resources {
