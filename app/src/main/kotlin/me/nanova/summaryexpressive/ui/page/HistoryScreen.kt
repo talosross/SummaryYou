08dad9cfa63aa0342c92299108d4787b8beeb4f4
alex <kid1412621@gmail.com>
2025-07-24 14:08:14 +0800

    fix: history not show

--- app/src/main/kotlin/me/nanova/summaryexpressive/ui/page/HistoryScreen.kt
+++ app/src/main/kotlin/me/nanova/summaryexpressive/ui/page/HistoryScreen.kt
@@ -3,6 +3,7 @@ package me.nanova.summaryexpressive.ui.page
 import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -49,6 +50,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import me.nanova.summaryexpressive.R
+import me.nanova.summaryexpressive.model.TextSummary
 import me.nanova.summaryexpressive.ui.component.SummaryCard
 import me.nanova.summaryexpressive.vm.SummaryViewModel
 
@@ -61,70 +63,184 @@ fun HistoryScreen(
     val scrollBehavior =
         TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
     var searchMode by remember { mutableStateOf(false) } // Active state for SearchBar
+    val haptics = LocalHapticFeedback.current
+    val context = LocalContext.current
+
+    if (searchMode) {
+        SearchView(onExitSearch = { searchMode = false })
+        return
+    }
+
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            LargeTopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(),
+                title = {
+                    Text(
+                        text = stringResource(id = R.string.history),
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigate("home") }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Go Back"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { searchMode = true }
+                    ) {
+                        Icon(
+                            Icons.Outlined.Search,
+                            contentDescription = stringResource(id = R.string.search),
+                            modifier = Modifier.size(24.dp)
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior
+            )
+        },
+    ) { innerPadding ->
+        val summaries = viewModel.textSummaries.reversed()
+
+        LazyColumn(
+            modifier = Modifier
+                .padding(start = 20.dp, end = 20.dp)
+                .fillMaxSize(),
+            contentPadding = innerPadding,
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            summaries.takeIf { it.isNotEmpty() }?.let { list ->
+                items(items = list, key = { it.id }) {
+                    SummaryCard(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 15.dp),
+                        title = it.title,
+                        author = it.author,
+                        summary = it.text,
+                        isYouTube = it.youtubeLink,
+                        cardColors = CardDefaults.elevatedCardColors(
+                            containerColor = MaterialTheme.colorScheme.surfaceVariant
+                        ),
+                        onLongClick = {
+                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                            Toast
+                                .makeText(
+                                    context,
+                                    context.getString(R.string.deleted),
+                                    Toast.LENGTH_SHORT
+                                )
+                                .show()
+                            viewModel.removeTextSummary(it.id)
+                        }
+                    )
+                }
+            } ?: item {
+                Column {
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Text(
+                        text = stringResource(id = R.string.noHistory),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun SearchView(onExitSearch: () -> Unit, viewModel: SummaryViewModel = hiltViewModel()) {
+    val searchResults = remember { mutableStateListOf<TextSummary>() }
     var searchText by remember { mutableStateOf("") } // Query for SearchBar
     val focusManager = LocalFocusManager.current // Hide cursor
     val focusRequester = remember { FocusRequester() } // Show cursor after removing
     val haptics = LocalHapticFeedback.current
     val context = LocalContext.current
 
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
 
-    if (!searchMode) {
-        Scaffold(
-            modifier = Modifier
-                .fillMaxSize()
-                .nestedScroll(scrollBehavior.nestedScrollConnection),
-            topBar = {
-                LargeTopAppBar(
-                    colors = TopAppBarDefaults.topAppBarColors(),
-                    title = {
-                        Text(
-                            text = stringResource(id = R.string.history),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis
-                        )
+    Box(modifier = Modifier.fillMaxSize()) {
+        SearchBar(
+            inputField = {
+                SearchBarDefaults.InputField(
+                    query = searchText,
+                    onQueryChange = {
+                        searchText = it
+                        searchResults.clear()
+                        val resultIds = viewModel.searchTextSummary(searchText).reversed()
+                        val summariesMap =
+                            viewModel.textSummaries.associateBy { summary -> summary.id }
+                        searchResults.addAll(resultIds.mapNotNull { id -> summariesMap[id] })
                     },
-                    navigationIcon = {
-                        IconButton(onClick = { navController.navigate("home") }) {
+                    onSearch = { focusManager.clearFocus() },
+                    expanded = true,
+                    onExpandedChange = { active -> if (!active) onExitSearch() },
+                    placeholder = {
+                        Text(text = stringResource(id = R.string.search))
+                    },
+                    leadingIcon = {
+                        IconButton(onClick = onExitSearch) {
                             Icon(
                                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = "Localized description"
+                                contentDescription = "Back"
                             )
                         }
                     },
-                    actions = {
+                    trailingIcon = {
                         IconButton(
-                            onClick = { searchMode = true }
+                            onClick = {
+                                if (searchText.isBlank()) {
+                                    onExitSearch()
+                                } else {
+                                    searchText = ""
+                                    focusRequester.requestFocus()
+                                }
+                            }
                         ) {
-                            Icon(
-                                Icons.Outlined.Search,
-                                contentDescription = stringResource(id = R.string.search),
-                                modifier = Modifier.size(24.dp)
-                            )
+                            Icon(imageVector = Icons.Default.Clear, contentDescription = "Clear")
                         }
                     },
-                    scrollBehavior = scrollBehavior
                 )
             },
-        ) { innerPadding ->
-            val reversedList = remember { mutableStateListOf<String>() }
-
-            LaunchedEffect(viewModel.textSummaries) {
-                reversedList.clear()
-                reversedList.addAll(viewModel.getAllTextSummaries().reversed())
-            }
-
-            LazyColumn(
-                modifier = Modifier
-                    .padding(start = 20.dp, end = 20.dp)
-                    .fillMaxSize(),
-                contentPadding = innerPadding,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                if (viewModel.textSummaries.isNotEmpty()) {
-                    items(reversedList) { textSummaryId ->
-                        val textSummary =
-                            viewModel.textSummaries.firstOrNull { it.id == textSummaryId }
-                        if (textSummary != null) {
+            expanded = true,
+            onExpandedChange = { active -> if (!active) onExitSearch() },
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .focusRequester(focusRequester)
+                .fillMaxWidth(),
+            shape = SearchBarDefaults.inputFieldShape,
+            colors = SearchBarDefaults.colors(),
+            tonalElevation = SearchBarDefaults.TonalElevation,
+            shadowElevation = SearchBarDefaults.ShadowElevation,
+            windowInsets = SearchBarDefaults.windowInsets,
+            content = {
+                if (searchText.isNotEmpty() && searchResults.isEmpty()) {
+                    Text(
+                        text = stringResource(id = R.string.nothingFound),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                } else {
+                    LazyColumn(
+                        modifier = Modifier
+                            .padding(start = 20.dp, end = 20.dp)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        items(items = searchResults, key = { it.id }) { textSummary ->
                             SummaryCard(
                                 modifier = Modifier
                                     .fillMaxWidth()
@@ -134,7 +250,7 @@ fun HistoryScreen(
                                 summary = textSummary.text,
                                 isYouTube = textSummary.youtubeLink,
                                 cardColors = CardDefaults.elevatedCardColors(
-                                    containerColor = MaterialTheme.colorScheme.surfaceVariant
+                                    containerColor = MaterialTheme.colorScheme.tertiaryContainer
                                 ),
                                 onLongClick = {
                                     haptics.performHapticFeedback(HapticFeedbackType.LongPress)
@@ -150,122 +266,8 @@ fun HistoryScreen(
                             )
                         }
                     }
-                } else {
-                    item {
-                        Spacer(modifier = Modifier.height(16.dp))
-                    }
-                    item {
-                        Text(
-                            text = stringResource(id = R.string.noHistory),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                        )
-                    }
                 }
-            }
-        }
-    } else {
-        val searchResults = remember { mutableStateListOf<String>() }
-        val onActiveChange: (Boolean) -> Unit = { searchMode = it }
-        Box(modifier = Modifier.fillMaxSize()) {
-            SearchBar(
-                inputField = {
-                    SearchBarDefaults.InputField(
-                        query = searchText,
-                        onQueryChange = {
-                            searchText = it
-                            searchResults.clear()
-                            searchResults.addAll(viewModel.searchTextSummary(searchText).reversed())
-                        },
-                        onSearch = { focusManager.clearFocus() },
-                        expanded = searchMode,
-                        onExpandedChange = onActiveChange,
-                        placeholder = {
-                            Text(text = stringResource(id = R.string.search))
-                        },
-                        leadingIcon = {
-                            IconButton(onClick = { searchMode = false }) {
-                                Icon(
-                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                    contentDescription = "Back"
-                                )
-                            }
-                        },
-                        trailingIcon = {
-                            IconButton(
-                                onClick = {
-                                    if (searchText.isBlank()) {
-                                        searchMode = false
-                                    } else {
-                                        searchText = ""
-                                        focusRequester.requestFocus()
-                                    }
-                                }
-                            ) {
-                                Icon(imageVector = Icons.Default.Clear, contentDescription = "Clear")
-                            }
-                        },
-                    )
-                },
-                expanded = searchMode,
-                onExpandedChange = onActiveChange,
-                modifier = Modifier
-                    .align(Alignment.TopCenter)
-                    .focusRequester(focusRequester)
-                    .fillMaxWidth(),
-                shape = SearchBarDefaults.inputFieldShape,
-                colors = SearchBarDefaults.colors(),
-                tonalElevation = SearchBarDefaults.TonalElevation,
-                shadowElevation = SearchBarDefaults.ShadowElevation,
-                windowInsets = SearchBarDefaults.windowInsets,
-                content = {
-                    if (searchText.isNotEmpty() && searchResults.isEmpty()) {
-                        Text(
-                            text = stringResource(id = R.string.nothingFound),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(16.dp)
-                        )
-                    } else {
-                        LazyColumn(
-                            modifier = Modifier
-                                .padding(start = 20.dp, end = 20.dp)
-                                .fillMaxSize(),
-                            verticalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            items(searchResults) { textSummaryId ->
-                                val textSummary =
-                                    viewModel.textSummaries.firstOrNull { it.id == textSummaryId }
-                                if (textSummary != null) {
-                                    SummaryCard(
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(vertical = 15.dp),
-                                        title = textSummary.title,
-                                        author = textSummary.author,
-                                        summary = textSummary.text,
-                                        isYouTube = textSummary.youtubeLink,
-                                        cardColors = CardDefaults.elevatedCardColors(
-                                            containerColor = MaterialTheme.colorScheme.tertiaryContainer
-                                        ),
-                                        onLongClick = {
-                                            haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                                            Toast
-                                                .makeText(
-                                                    context,
-                                                    context.getString(R.string.deleted),
-                                                    Toast.LENGTH_SHORT
-                                                )
-                                                .show()
-                                            viewModel.removeTextSummary(textSummary.id)
-                                        }
-                                    )
-                                }
-                            }
-                        }
-                    }
-                },
-            )
-        }
+            },
+        )
     }
 }
